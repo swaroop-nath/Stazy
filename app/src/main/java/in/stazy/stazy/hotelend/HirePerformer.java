1f5d444ae79b3d77d1e9301e0f6dce5c18d67e11
swaroop-nath <swaroopnath6@gmail.com>
2019-02-13 23:59:03 +0530

    Initial Commit - Some Activities Created and Layout of MainActivityHotel partially defined

---
+++ app/src/main/java/in/stazy/stazy/hotelend/HirePerformer.java
@@ -0,0 +1,15 @@
+package in.stazy.stazy.hotelend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.stazy.stazy.R;
+
+public class HirePerformer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hire_performer);
+    }
+}
